7bf94655dda2705c1a143c26b2a572bfcc1cffe6
Julian Rubisch <julian@julianrubisch.at>
2020-01-19 15:30:25 +0100

    Initial commit

---
+++ lib/attractor/ruby.rb
@@ -0,0 +1,8 @@
+require "attractor/ruby/version"
+
+module Attractor
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/attractor/ruby/version.rb
@@ -0,0 +1,5 @@
+module Attractor
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
